55a5f233b6e55fc2a56003736bbd55337a53896b
Andrey Reutov <reutov.andrey@gmail.com>
2019-10-30 19:26:26 +0300

    add tags searh

--- backend/controllers/recipes.js
+++ backend/controllers/recipes.js
@@ -8,6 +8,7 @@ async function getRecipes(ctx)
     const reqObj = {};
     if (query) reqObj.$text = {$search: query};
     if (age) reqObj.allowedAge = { $lte: age }
+    if (tags) reqObj.tags = { $elemMatch: { tagName: id1 } } //{ $all: tags }
 
     const recipes = await Recipe
         .find(reqObj, {score: {$meta: 'textScore'}})
