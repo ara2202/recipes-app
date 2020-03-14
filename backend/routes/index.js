7ab509f69ecd32f20239ad1dd28347e7c8802aee
Andrey Reutov <a.reutov@rambler-co.ru>
2020-03-14 22:55:28 +0300

    backend: added tagCategories API

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,7 @@ const Router = require('koa-router');
 const {getRecipes, getRecipeById} = require('../controllers/recipes');
 const {getProductDisplayNames} = require('../controllers/productDisplayNames');
 const {getTags} = require('../controllers/tags');
+const {getTagCategories} = require('../controllers/tagCategories');
 
 const router = new Router({prefix: '/api'});
 
@@ -9,5 +10,6 @@ router.get('/recipes', getRecipes);
 router.get('/recipe', getRecipeById);
 router.get('/productDisplayNames', getProductDisplayNames);
 router.get('/tags', getTags);
+router.get('/tagCategories', getTagCategories);
 
 module.exports = router;
